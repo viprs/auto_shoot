cddb68065f2e49e5013d676141a21fce9c62d491
viprs <viprs@qq.com>
2017-01-20 16:02:38 +0800

    移动函数的位置，删除冗余代码

--- lib/common.py
+++ lib/common.py
@@ -6,6 +6,11 @@ import os
 
 
 def get_user_dict_from_file(file_path):
+    """
+    :desc 获取用户列表
+    :param file_path:
+    :return:
+    """
     if not os.path.exists(file_path):
         return
     user_dict = {}
@@ -14,3 +19,20 @@ def get_user_dict_from_file(file_path):
             username, password = i.split()
             user_dict[username] = password
     return user_dict
+
+
+
+def get_fake_company(file_path):
+    """
+    :Desc: 获取排除公司列表，通常是一些皮包公司
+    :param file_path
+    :return:返回公司名称的unicode编码
+    """
+    if not os.path.exists(file_path):
+        return
+    fake_list = []
+    with open(file_path) as f:
+        for line in f:
+            fake_list.append(line.strip().decode("utf-8"))
+    assert fake_list != []
+    return fake_list
--- main.py
+++ main.py
@@ -5,84 +5,10 @@ from lib.common import get_user_dict_from_file
 from lib.Cjol import Cjol
 from lib.Zhilian import Zhilian
 from lib.Job51 import Job51
-from selenium import webdriver
 import logging
 import traceback
-import time,datetime
-import linecache
+import time, datetime
 from __builtin__ import str
-
-#设置需要投递几页的公司
-JOB_PAGE_NUM=2 #设置投递公司页数
-JOB_CITY= u"深圳"
-JOB_NAME= u"软件测试工程师"
-
-d1 = datetime.datetime.now()
-
-
-#获取排除公司列表，通常是一些皮包公司
-def get_company_info():
-    with open('exclude_company.txt','r') as info:
-        company_info=[]
-        for line in info:
-            line=line.strip()
-            company_info.append(line)
-    return company_info
-
-
-def search_job():
-    driver.find_element_by_id("JobLocation").clear()
-    driver.find_element_by_id("JobLocation").send_keys(JOB_CITY)
-    driver.find_element_by_xpath(".//*[@id='frmSearch']/div/div[3]/input").clear()
-    driver.find_element_by_xpath(".//*[@id='frmSearch']/div/div[3]/input").send_keys(JOB_NAME)
-    driver.find_element_by_id("search").click()
-    #防止第二个浏览器窗口延迟加载
-    now_handle = driver.current_window_handle
-    time.sleep(3)
-    all_handles = driver.window_handles
-    for handle in all_handles:
-        if handle != now_handle:
-            driver.switch_to_window(handle)
-
-
-def screen_company():
-    job_checkbox=driver.find_elements_by_css_selector("input[type=checkbox]")
-    gsmc=driver.find_elements_by_class_name("gsmc")
-    company_count=len(gsmc)
-    company_info=get_company_info()
-    for select_num in range(1,company_count):
-        #去除匹配公司多选框
-        if (gsmc[select_num].text.encode('utf-8') in company_info)==False:
-            job_checkbox[select_num+1].click()
-        if select_num % 10 ==0:
-            select_check_job()
-            driver.find_element_by_id("checkbox4al2").click()
-            time.sleep(1)
-            driver.find_element_by_id("checkbox4al2").click()
-            time.sleep(2)
-
-def select_check_job():
-    try:
-        #点击申请工作按钮
-        driver.find_element_by_link_text(u"申请职位").click()
-    except:pass
-    try:
-    #点击投递广告页面 忽略按钮
-        pass
-    except:pass
-    try:
-        #点击立即申请按钮
-        driver.find_element_by_id("applynowbutton").click()
-    except:pass
-    try:
-        #关闭已经申请过的职位弹窗
-        driver.find_element_by_css_selector(u"a[title=\"关闭\"] > img").click()
-    except:pass
-    try:
-        #关闭微信广告页面
-        driver.find_element_by_css_selector("span.fr.popup_close").click()
-    except:pass
-
 
 
 def cjol_shoot(username, password):
@@ -114,6 +40,7 @@ def job51_shoot(username, password):
     zl.custom_select_job()
 
 if __name__=="__main__":
+    d1 = datetime.datetime.now()
     #生成日志文件
     ZHAOPIN_SITE = {
         'zhilian': zhilian_shoot
